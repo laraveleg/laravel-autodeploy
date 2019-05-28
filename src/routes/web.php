bb4565aef05df00b46ddc92a6f99b8bb49b3d846
Karim Mohamed <komicho1996@gmail.com>
2019-05-29 00:56:41 +0200

    Remove web middleware

--- src/routes/web.php
+++ src/routes/web.php
@@ -2,7 +2,7 @@
 Route::group([
     'prefix' => 'komicho',
     'namespace' => 'Komicho\Laravel\AutoDeploy\app\Http\Controllers',
-    'middleware' => 'web'
+    // 'middleware' => 'web'
 ], function () {
     Route::post('/deploy', 'AutoDeployController@deploy');
 });
