3a5207933d380df9b74cc221667c414533142f2b
Karim Mohamed <komicho1996@gmail.com>
2020-06-10 17:43:23 +0200

    Check header
    Check Content-Type header for show git bar on `text/html` only

--- src/app/Http/Middleware/LastCommitMiddleware.php
+++ src/app/Http/Middleware/LastCommitMiddleware.php
@@ -25,26 +25,32 @@ class LastCommitMiddleware
     {
         $environment = \App::environment();
 
-        if ($environment != 'production') {
-            $last_commit = new Process(['git', 'log', '--format="%H"', '-n', '1']);
-            $last_commit->setWorkingDirectory(base_path());
-            $last_commit->run();
-    
-            $last_commit_time = new Process(['git', 'log', '-1', '--format=%cd']);
-            $last_commit_time->setWorkingDirectory(base_path());
-            $last_commit_time->run();
-    
-            $current_branch = new Process(['git', 'rev-parse', '--abbrev-ref', 'HEAD']);
-            $current_branch->setWorkingDirectory(base_path());
-            $current_branch->run();
-    
-            echo view('LaravelEG_AutoDeploy::git-bar')->with([
-                'last_commit' => $last_commit->getOutput(),
-                'last_commit_time' => $last_commit_time->getOutput(),
-                'current_branch' => $current_branch->getOutput()
-            ]);
-        }
+        $response = $next($request);
+
+        $header_content_type = $response->headers->get('Content-Type');
+
+        if (strpos($header_content_type, 'text/html') !== false) {
+            if ($environment != 'production') {
+                $last_commit = new Process(['git', 'log', '--format="%H"', '-n', '1']);
+                $last_commit->setWorkingDirectory(base_path());
+                $last_commit->run();
         
-        return $next($request);
+                $last_commit_time = new Process(['git', 'log', '-1', '--format=%cd']);
+                $last_commit_time->setWorkingDirectory(base_path());
+                $last_commit_time->run();
+        
+                $current_branch = new Process(['git', 'rev-parse', '--abbrev-ref', 'HEAD']);
+                $current_branch->setWorkingDirectory(base_path());
+                $current_branch->run();
+        
+                echo view('LaravelEG_AutoDeploy::git-bar')->with([
+                    'last_commit' => $last_commit->getOutput(),
+                    'last_commit_time' => $last_commit_time->getOutput(),
+                    'current_branch' => $current_branch->getOutput()
+                ]);
+            }
+        }
+
+        return $response;
     }
 }
